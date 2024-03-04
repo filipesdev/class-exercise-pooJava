095e97fe38adff75dc8e510005650d45e3145466
Filipemtb <filipera3@gmail.com>
2024-03-04 15:13:31 -0300

    intanciando o Instant.now com o horário GMT

--- exercicio 7/src/application/Main.java
+++ exercicio 7/src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -9,7 +10,10 @@ public class Main {
         /* LocalDate d01 = LocalDate.now();
         System.out.println(d01); */
 
-        LocalDateTime d02 = LocalDateTime.now();
-        System.out.println(d02);
+        /* LocalDateTime d02 = LocalDateTime.now();
+        System.out.println(d02); */
+
+        Instant d03 = Instant.now();
+        System.out.println(d03);
     }
 }
