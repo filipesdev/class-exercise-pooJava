6030c6feb437c0bbf56ebcb65abc19618193863a
Filipemtb <filipera3@gmail.com>
2024-03-04 15:57:11 -0300

    transformando Data-Hora em texto costumizado usando o DateTimeFormatter

---
+++ exercicio 8/src/application/Main.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // transformando Data-Hora em texto costumizado
+
+        LocalDate d04 = LocalDate.parse("2024-03-04");
+        LocalDateTime d05 = LocalDateTime.parse("2024-03-04T15:50:30");
+        Instant d06 = Instant.parse("2024-03-04T15:50:30Z");
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println("d04 = " + d04.format(fmt1));
+        System.out.println("d04 = " + fmt1.format(d04));
+        System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+    }
+}
