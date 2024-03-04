9ea3d72d9fc8cf2a4617927fbe4624308f716540
Filipemtb <filipera3@gmail.com>
2024-03-04 15:16:23 -0300

    intanciando um texto ISO 8601 e gerando Data-Hora apartir dele

--- exercicio 7/src/application/Main.java
+++ exercicio 7/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,7 +14,10 @@ public class Main {
         /* LocalDateTime d02 = LocalDateTime.now();
         System.out.println(d02); */
 
-        Instant d03 = Instant.now();
-        System.out.println(d03);
+        /* Instant d03 = Instant.now();
+        System.out.println(d03); */
+
+        LocalDate d04 = LocalDate.parse("2024-03-04");
+        System.out.println(d04);
     }
 }
